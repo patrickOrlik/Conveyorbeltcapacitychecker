6dd06a8a7eb8e9870c08d2ea433fd0ed124740f3
Your Full Name <patrickmathiasorlik@gmail.com>
2025-09-08 21:39:28 +0200

    added a if statements that check if the input is a numeric value

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int Motors;
-int weight;
+float weight;
 
 int main(void) {
   printf("How many motors are carrying the package?: ");
-  scanf("%d", &Motors);
+  if (scanf("%d", &Motors) != 1) {
+    printf("Input is wrong please enter a digit!");
+    return 0;
+  }
   printf("how many kg of packages do we expect?:");
-  scanf("%d", &weight);
-
-  if (5.6 < (float)weight / (float)Motors)
+  if (scanf("%f", &weight) != 1) {
+    printf("input is wrong please enter a numeric value!");
+    return 0;
+  }
+  if (5.6 < weight / Motors)
     printf("sorry the weight is too large");
   else
     printf("This will work!");
