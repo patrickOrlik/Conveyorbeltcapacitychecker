5db1c8afa0245745c45530e410e668e1c7e987d8
Your Full Name <patrickmathiasorlik@gmail.com>
2025-09-10 15:25:59 +0200

    added comments to the code

--- main.c
+++ main.c
@@ -2,20 +2,27 @@
 
 int Motors;
 float weight;
-
+int input;
 int main(void) {
+  // asks for the amount of motors and also makes sure that you enter a
+  // numerical value
   printf("How many motors are carrying the package?: ");
   if (scanf("%d", &Motors) != 1) {
     printf("Input is wrong please enter a digit!");
     return 0;
   }
+  // asks for the amount of weight we expect and also makes sure you enter a
+  // numerical value
   printf("how many kg of packages do we expect?:");
   if (scanf("%f", &weight) != 1) {
     printf("input is wrong please enter a numeric value!");
     return 0;
   }
+  // calculates if the amount of weight i too large compared to the amount of
+  // motors
   if (5.6 < weight / Motors)
     printf("sorry the weight is too large");
+
   else
     printf("This will work!");
 }
